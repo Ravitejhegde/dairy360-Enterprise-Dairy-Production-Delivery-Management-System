2cfd5d9a660bbace82cdb4b49aa6ebef05eee715
Ravitejhegde <hegderaviteja21@gmail.com>
2026-06-06 23:08:48 +0530

    products page updated

--- src/app/features/admin/products/products.ts
+++ src/app/features/admin/products/products.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './products.html',
-  styleUrl: './products.css',
+  styleUrl: './products.css'
 })
 export class Products {
 
+  newProductName = '';
+
+  newProductPrice = 0;
+
+  newProductQuantity = 0;
+
   products = [
+
     {
       productName: 'Milk',
       unitPrice: 50,
@@ -20,19 +28,25 @@ export class Products {
       productName: 'Curd',
       unitPrice: 60,
       availableQuantity: 200
-    },
-
-    {
-      productName: 'Paneer',
-      unitPrice: 350,
-      availableQuantity: 50
-    },
-
-    {
-      productName: 'Butter',
-      unitPrice: 450,
-      availableQuantity: 25
     }
+
   ];
+
+  addProduct() {
+
+    this.products.push({
+
+      productName: this.newProductName,
+
+      unitPrice: this.newProductPrice,
+
+      availableQuantity: this.newProductQuantity
+
+    });
+     this.newProductName = '';
+    this.newProductPrice = 0;
+    this.newProductQuantity = 0;
+
+  }
 
 }
