dde22ac5c47d74120443fdb30b84109bcbce6b20
Ravitejhegde <hegderaviteja21@gmail.com>
2026-06-06 21:36:03 +0530

    Add admin sidebar layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Dashboard } from './features/admin/dashboard/dashboard';
+import { Sidebar } from './core/sidebar/sidebar';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dashboard],
+  imports: [RouterOutlet, Dashboard, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
